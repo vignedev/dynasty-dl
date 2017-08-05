b085464f5300ad816d5f9b43e96c00cfa525ef35
vignedev <vignedev@gmail.com>
2017-08-05 18:22:03 +0200

    Complete rewrite
    The inner code is different, but the outer shell is the same.
    Replaced jimp with pngjs, since only PNGs are causing problems in PDFs.
    Sprinkled with promises.

--- index.js
+++ index.js
@@ -1,174 +1,149 @@
-/*
-dynasty-dl by vignedev
-*/
+const BASEURL = 'https://dynasty-scans.com'
 
-const https = require('https'),
-	cheerio = require('cheerio'),
-	fs = require('fs'),
-	argv = require('commander'),
-	appendix = 'https://dynasty-scans.com',
-	path = require('path'),
-	pdfkit = require('pdfkit'),
-	progress = require('progress'),
-	imageSize = require('imagesize')
-	jimp = require('jimp') || null
+const fs = require('fs')
+const https = require('https')
+const cheerio = require('cheerio')
+const path = require('path')
+const pdfkit = require('pdfkit')
+const imageSize = require('imageSize')
+const progress = require('progress')
 
-//pdf document, just a storage
-var doc
-argv
+const PNG = require('pngjs').PNG
+
+const argv = require('commander')
+	.version(require('./package.json').version)
 	.arguments('<url>')
-	.description('A tool for downloading manga from Dynasty-Scans.\n  NOTE: If you\'re experiencing image corruption in PDFs, try using the -r flag with jimp installed.')
-	.option('-c, --chapters <start>-<end>', 'range of chapters or individual chapters', (val) => {return val.split('-').map(Number)})
-	.option('-p, --generate_pdf', 'generate a pdf from the images (this deletes the page images)')
-	.option('-r, --reconvert', 'reconvert pages into PNG, PDF generation is enabled by default')
-	.action((url) => {
-		if(argv.chapters && (argv.chapters[1] < argv.chapters[0]))
-			throw new Error('End range cannot be bigger than start range!')
-		var manga = {
-			url: url,
-			type: (url.includes('series') ? 'series' : 'chapter'), //assume that the url is okay and dandy.
-			chapters: (argv.chapters ? (argv.chapters.length == 1 ? argv.chapters : Array.from({length: (argv.chapters[1]+1 - argv.chapters[0])}, (v, k) => k + argv.chapters[0])) : [])
+	.description('Simple tool for batch-downloading from Dynasty-Scans. Supports also PDF saving as well as chapter selection.')
+	.option('-c, --chapters <a>-<b>', 'Chapter range.')
+	.option('-C, --listChapters', 'List all chapters with correct indexes.')
+	.option('-o, --output [path]', 'Different output path, defaults to current working directory.')
+	.option('-p, --pdf', 'Downloads pdf instead of seperated images.')
+	.option('-n, --noconvert', 'Skips PNG to PDF coversion.')
+	.parse(process.argv)
+
+if(!argv.args[0]) argv.help()
+
+var config = {
+	pdf: argv.pdf,
+	outputDir: argv.output || process.cwd()
+}
+
+parseManga({
+	url: argv.args[0],
+	isSeries: argv.args[0].includes('series'),
+	chapters: argv.chapters
+})
+
+async function parseManga(manga){
+	if(manga.isSeries){
+		var sBody = cheerio.load(await get(manga.url)), cPath = config.outputDir+'/', doc
+
+		console.log(`\n  ${sBody('.tag-title > b').text()}`)
+
+		var mangaInfo = {
+			title: legalize(sBody('.tag-title > b').text()),
+			chapters: sBody('a.name').map((i, url) => {
+				return {
+					url: BASEURL+url.attribs.href,
+					name: url.children[0].data
+				}
+			}).toArray()
 		}
-		startParsing(manga);
-	})
-	.parse(process.argv)	//process.argv
 
-function startParsing(manga){
-	if(argv.reconvert) argv.generate_pdf = true
-
-	if(manga.type == 'series'){
-		//find it, parse the chapters and manually download them
-		get(manga.url, body => {
-			var pBody = cheerio.load(body)
-			var mangaInfo = {
-				title: pBody('.tag-title > b').text(),
-				author: pBody('.tag-title > a').text(),
-				chapters: pBody('a.name').map((i, url) => {return {url:appendix+url.attribs.href,name:url.children[0].data}}).toArray()
+		if(argv.listChapters){
+			for(var i = 0; i < mangaInfo.chapters.length; i++){
+				console.log(`  ${i}: ${mangaInfo.chapters[i].name}`)
 			}
+			process.exit(0)
+		}
 
-			//at this point, mangaInfo contains all chapters, filter it if user wants to
-			if(manga.chapters.length != 0){
-				//get them by indexes
-				var filtered = []
-				manga.chapters.forEach(cIndex => {
-					var cInx = cIndex-1 //because i haven't decided if I wanted them zero-indexed or not.
-					if(mangaInfo.chapters[cInx]){
-						filtered.push(mangaInfo.chapters[cInx])
-					}else{
-						throw new Error(`Chapter ${cIndex} not found.`)
-					}
-				})
-				mangaInfo.chapters = filtered
+		//slice chapters that are needed and stuff
+		if(manga.chapters){
+			let selection = argv.chapters.split('-')
+			if(selection.length == 1){
+				mangaInfo.chapters = [mangaInfo.chapters[selection[0]]]
+			}else if(selection.length == 2){
+				mangaInfo.chapters = mangaInfo.chapters.slice(selection[0], selection[1])
 			}
+		}
 
-			//Here we have the final mangaInfo, ready to be parsed
-			// >checked, everything seems to be working correctly here, onwards to the "download that shit"
-
-			//generate a pdf and pipe it
-			if(argv.generate_pdf){
-				doc = new pdfkit({autoFirstPage: false})
-				doc.pipe(fs.createWriteStream(`${process.cwd()}/${legalize(mangaInfo.title+((argv.chapters) ? (' ch'+argv.chapters[0] + (argv.chapters[1] ? '-'+argv.chapters[1] : '')) : ''))}.pdf`))
-			}
-
-			var dir = process.cwd() + '/' + legalize(mangaInfo.title)
-			if(!argv.generate_pdf) mkdir(dir)
-
-			var indx = 0;
-			function download(){
-				if(indx < mangaInfo.chapters.length){
-					console.log('\n'+mangaInfo.chapters[indx].name)
-					var chapDir = dir + '/' + legalize(mangaInfo.chapters[indx].name)
-					if(!argv.generate_pdf) mkdir(chapDir)
-					downloadChapter(mangaInfo.chapters[indx].url, false, chapDir, () => {
-						indx++; download()
-					})
-				}else{
-					if(argv.generate_pdf) doc.end()
+		if(config.pdf){
+			doc = new pdfkit({autoFirstPage: false})
+			doc.pipe(fs.createWriteStream(cPath+mangaInfo.title+'.pdf'))
+		}else{
+			cPath += mangaInfo.title+'/'
+			mkdir(cPath);
+		}
+		for(var i = 0; i < mangaInfo.chapters.length; i++){
+			let chapter = await downloadChapter(mangaInfo.chapters[i].url)
+			if(config.pdf){
+				for(var y = 0; y < chapter.images.length; y++){
+					let image = chapter.images[y]
+					doc.addPage({size: [image.size.width, image.size.height]})
+					if(image.size.format == 'png' && argv.noconvert) image.buffer = await reconvertPNG(image.buffer)
+					doc.image(image.buffer, 0, 0)
+				}
+			}else{
+				let tPath = cPath+chapter.name+'/'
+				mkdir(tPath)
+				for(var y = 0; y < chapter.images.length; y++){
+					let image = chapter.images[y]
+					await write(tPath+path.basename(image.image), image.buffer)
 				}
 			}
-			download()
-		})
-	}else if(manga.type == 'chapter'){
-		downloadChapter(manga.url, true, process.cwd(),() => {
-			if(argv.generate_pdf) doc.end()
-		})
+		}
+		if(config.pdf) doc.end()
+	}else{
+		let chapter = await downloadChapter(manga.url)
+
+		let cPath = config.outputDir+'/'
+		if(config.pdf){
+			var doc = new pdfkit({autoFirstPage: false})
+			doc.pipe(fs.createWriteStream(cPath+chapter.name+'.pdf'))
+			for(var i = 0; i < chapter.images.length; i++){
+				let image = chapter.images[i]
+				doc.addPage({size: [image.size.width, image.size.height]})
+				if(image.size.format == 'png') image.buffer = await reconvertPNG(image.buffer)
+				doc.image(image.buffer, 0, 0)
+			}
+			doc.end()
+		}else{
+			mkdir(chapter.name); cPath += chapter.name+'/'
+			for(var i = 0; i < chapter.images.length; i++){
+				let image = chapter.images[i]
+				await write(cPath+path.basename(image.image), image.buffer)
+			}
+		}
 	}
 }
 
-function downloadChapter(chapterURL, singleChapter, dir, cb){
-	get(chapterURL, body => {
-		var pBody = cheerio.load(body)
-		var json = JSON.parse(pBody('body > script').text().match(/\[(.*)\]/)[0])
-		var title = null
-
-		if(singleChapter){
-			title = pBody('#chapter-title > b').text()
-			console.log('\n'+title)
-			if(argv.generate_pdf){
-				doc = new pdfkit({autoFirstPage: false})
-				doc.pipe(fs.createWriteStream(`${process.cwd()}/${legalize(title)}.pdf`))
-			}else{
-				mkdir(dir+'/'+legalize(title))
-				dir += '/'+legalize(title)
+function downloadChapter(url){
+	return new Promise((resolve, reject) => {
+		get(url).then(async body => {
+			try{
+				var cBody = cheerio.load(body), name = cBody('#chapter-title > b').text()
+				var images = JSON.parse(cBody('body > script').html().match(/\[(.*)\]/)[0])
+				console.log(`\n  ${name}`)
+				var progressBar = new progress('  [:bar] :percent', {
+					complete: '=',
+					incomplete: ' ',
+					width: images.length,
+					total: images.length
+				})
+				for(var i = 0; i < images.length; i++){
+					let imgData = await get(BASEURL+images[i].image, true)
+					images[i].buffer = imgData.buffer
+					images[i].size = imgData.size
+					progressBar.tick()
+				}
+				resolve({
+					name: legalize(name),
+					images: images
+				})
+			}catch(err){
+				reject(err)
 			}
-		}
-		
-		var progressBar = new progress('  (:current/:total) [:bar] :percent', {total:json.length})
-
-		var indx = 0
-		function download(){
-			if(indx < json.length){
-				var current = json[indx]
-				var filename = legalize(decodeURI(path.basename(current.image)))
-				var saveLoc = dir+'/'+filename
-				//console.log(`  Saved ${saveLoc}`); indx++; download()	//debug
-				if(argv.generate_pdf){
-					https.get(appendix+'/'+current.image, res => {
-						var dimensions = []
-						imageSize(res, (err,dim) => {dimensions = dim})
-
-						var cap = []
-						res.on('data', chunk => {cap.push(chunk)})
-						res.on('end', () => {
-							doc.addPage({size: [dimensions.width,dimensions.height]})
-							if(argv.reconvert){
-								if(jimp){
-									jimp.read(Buffer.concat(cap), (err,image) => {
-										if(err) throw err
-										image.getBuffer(jimp.MIME_PNG, (err,result) => {
-											if(err) throw err
-											doc.image(result, 0, 0)
-											progressBar.tick()
-											indx++
-											download();
-										})
-									})
-								}else{
-									throw new Error('Jimp is not installed.\nUse npm i jimp -g to install it and try again.\n')
-									process.exit(1)
-								}
-							}else{
-								doc.image(Buffer.concat(cap), 0, 0)
-								progressBar.tick()
-								indx++
-								download();
-							}
-						})
-					})
-				}else{
-					pipe(appendix+'/'+current.image, saveLoc, () => {
-						//console.log(`  Saved ${filename}`)
-						progressBar.tick()
-						indx++
-						download(); 
-					})
-				}/**/
-			}else{
-				cb()
-			}
-		}
-
-		download()
+		})
 	})
 }
 
@@ -177,30 +152,80 @@ function mkdir(path){
 	if (!fs.existsSync(path))
 		fs.mkdirSync(path)
 }
-function get(url, cb){
-	https.get(url, (res) => {
-		var capture = ''
-		res.on('data', (chunk) => {capture += chunk})
-		res.on('end', () => {cb(capture, res.statusCode)})
+
+function pipe(url, path){
+	return new Promise((resolve, reject) => {
+		https.get(url, res => {
+			res.pipe(fs.createWriteStream(path))
+			res.on('end', resolve)
+			res.on('error', reject)
+		})
 	})
 }
 
-/*function getBuffer(url, cb){
-	https.get(url, (res) => {
-		var capture = []
-		res.on('data', (chunk) => {capture.push(chunk)})
-		res.on('end', () => {cb(Buffer.concat(capture), res.statusCode)})
-	})
-}*/
-
-function pipe(url, path, cb){
-	https.get(url, res => {
-		res.pipe(fs.createWriteStream(path))
-		res.on('end', cb) 
+// in case we need buffer later on
+function get(url, buffer = false){
+	return new Promise((resolve, reject) => {
+		https.get(url, async res => {
+			var size, cap, lURL = url.toLowerCase()
+			if(lURL.endsWith('.gif') || lURL.endsWith('.png') || lURL.endsWith('.jpg') || lURL.endsWith('.jpeg')){
+				getImageSize(res).then(ssize => {
+					size = ssize
+				})
+			}
+			if(buffer){
+				cap = []
+				res.on('data', chunk => {cap.push(chunk)})
+				res.on('end', () => {
+					resolve(size ? {buffer: Buffer.concat(cap), size: size}: Buffer.concat(cap))
+				})
+			}else{
+				cap = ''
+				res.on('data', chunk => {cap += chunk})
+				res.on('end', () => {
+					resolve(cap)
+					//no point of including size in here, since who in the right mind would download images to string?
+				})
+			}
+		})
 	})
 }
 
-// some OS (eg. Windows) don't support those characters, and an error would be thrown when it would accur.
+function getImageSize(stream){
+	return new Promise((resolve, reject) => {
+		imageSize(stream, (err,res) => {
+			if(err) return reject(err)
+			resolve(res)
+		})
+	})
+}
+
+// some OS (eg. Windows) don't like them in the path name, so they throw a tantrum
 function legalize(text, replacer = ''){
 	return text.replace(/\\|\/|:|\*|\?|"|<|>/g, replacer)
+}
+
+// okay i might have gone overboard with promises
+function write(file, data){
+	return new Promise((resolve,reject) => {
+		fs.writeFile(file,data, (err) => {
+			if(err) return reject(err)
+			resolve()
+		})
+	})
+}
+
+function reconvertPNG(buffer){
+	return new Promise((resolve,reject) => {
+		let png = new PNG()
+		png.parse(buffer, (err,data) => {
+			if(err) return reject(err)
+			let stream = png.pack()
+			var cap = []
+			stream.on('data', chunk => {cap.push(chunk)})
+			stream.on('end', () => {
+				resolve(Buffer.concat(cap))
+			})
+		})
+	})
 }
